085211f39ccfb8c416956fb030d3e3c04e10528b
ARMx86-64 <gontarz.tmn.code@proton.me>
2025-12-30 00:05:56 +0100

    separated script for system update and update counter

---
+++ scripts/checkupdates.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# check arch release
+if [ ! -f /etc/arch-release ] ; then
+    exit 0
+fi
+
+# script path
+ScrDir=$(dirname "$(realpath "$0")")
+source $ScrDir/globals.sh
+
+# check for updates (packages)
+get_aurhelper
+
+ofc=`${aurhlpr} -Qua | wc -l`
+aur=`checkupdates | wc -l`
+
+# TODO: flatpaks
+
+# total updates
+all=$(( ofc + aur ))
+
+# tooltip
+if [ $all -eq 0 ] ; then
+    echo "{\"text\":\"$all \", \"tooltip\":\" 󰚰  All packages are up to date\"}"
+else
+    echo "{\"text\":\"$all \", \"tooltip\":\" 󰚰  Official: $ofc | AUR: $aur\"}"
+fi
